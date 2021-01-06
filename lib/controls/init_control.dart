1a2ee5325bb9aae3e4c2857135d6d26a4e409949
MartinKubela <kubelama@gmail.com>
2021-01-06 22:31:03 +0100

    meowing added

--- lib/controls/init_control.dart
+++ lib/controls/init_control.dart
@@ -1,11 +1,21 @@
 import 'package:app_meow/api/api.dart';
 import 'package:dio/dio.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:kiwi/kiwi.dart';
 
 class InitControl {
   InitControl() {
     final di = KiwiContainer();
     di.clear();
-   di.registerSingleton<Api>((c) => Api());
+    di.registerSingleton<Api>((c) => Api());
+
+  //  _setUpMeowing();
+  }
+
+  Future<void> _setUpMeowing() async {
+    final audioPlayer = AudioPlayer();
+    await audioPlayer.setLoopMode(LoopMode.one);
+    await audioPlayer.setAsset('assets/audio/meowing.mp3');
+    audioPlayer.play();
   }
 }
